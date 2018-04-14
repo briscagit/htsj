471c45332097f47066dc13212f6373717cf0bf38
briscagit <briscagit@gmail.com>
2018-04-14 16:05:32 -0400

    change variable names and function block

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,22 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'All hope abandon, ye who enter here.';
 */
-var myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('filter');
-    if(mySrc === 'invert(0%)') { // filter is off
-      myImage.setAttribute ('filter','invert(100%)');
+/*
+<h1 id="id1">My Heading 1</h1>
+
+<button type="button" 
+onclick="document.getElementById('id1').style.color = 'red'">
+Click Me!</button>
+*/
+
+var image = document.querySelector('img');
+
+image.onclick = function() {
+    var myProperty = image.style.filter;
+    if(myProperty === 'invert(0%)') { // filter is off
+      myProperty = 'invert(100%)';
     } else { // filter is on
-      myImage.setAttribute ('filter','invert(0%)');
+      myProperty = 'invert(0%)';
     }
 }
