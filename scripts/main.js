2e1a3ac5239ad05b9aaa10b2c4162e7529e1b501
briscagit <briscagit@gmail.com>
2018-04-14 15:03:04 -0400

    add image changer

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,13 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'All hope abandon, ye who enter here.';
 */
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('filter');
+    if(mySrc === 'invert(0%)') { // filter is off
+      myImage.setAttribute ('filter','invert(100%)');
+    } else { // filter is on
+      myImage.setAttribute ('filter','invert(0%)');
+    }
+}
